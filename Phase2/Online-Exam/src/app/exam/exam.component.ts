80a24a58bebea180bdac65db8abdd2d2c200a21f
Parsa <kashefiparsa@gmail.com>
2021-08-19 15:26:30 -0400

    finished online exam

--- Phase2/Online-Exam/src/app/exam/exam.component.ts
+++ Phase2/Online-Exam/src/app/exam/exam.component.ts
@@ -9,35 +9,58 @@ import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./exam.component.css']
 })
 export class ExamComponent implements OnInit {
-  questionArray:Array<questions>=[];
-  flag:number=0;
-  questionRef:FormGroup;
-  show:boolean=false;
-  constructor(public questionsSer:QuestionsService,fb:FormBuilder) { 
-    this.questionRef=fb.group({ans:""});
+  questionArray: Array<questions> = [];
+  flag: number = 0;
+  questionRef: FormGroup;
+  show: boolean = false;
+  showExam: boolean = true;
+  finalMessage: string = "";
+  showFinal: boolean = false;
+  passedExam: boolean = true;
+  percent: number = 0;
+  qNumbers:Array<boolean>=[true, true, true, true, true, true, true, true, true, true];
+  lastUsed:number=0;
+  constructor(public questionsSer: QuestionsService, fb: FormBuilder) {
+    this.questionRef = fb.group({ ans: "" });
   }
-  
+
   ngOnInit(): void {
-    this.questionsSer.getQuestionList().subscribe(result=>{
-      for(let item of result){
+    this.questionsSer.getQuestionList().subscribe(result => {
+      for (let item of result) {
         this.questionArray.push(item);
       }
     });
   }
-  checkAnswer(correctAns:string):void{
+  checkAnswer(correctAns: string, qNum:string): void {
     let ansValue = this.questionRef.value.ans;
-    if(ansValue==correctAns){
+    this.lastUsed=parseInt(qNum)-1;
+    if (ansValue == correctAns) {
       this.flag++;
     }
-  }
-  finalScore(){
-    let percent=(this.flag/10)*100;
-    if(percent>70){
-      alert(percent+"% \n PASS");
-    }
     else{
-      alert(percent+"% \n FAIL");
+      this.qNumbers[parseInt(qNum)-1] = false;
     }
-    this.show=true;
+  }
+  finalScore() {
+    if (confirm("Are you sure you want to submit the exam. All answers will be finalized!")) {
+      this.percent = (this.flag / 10) * 100;
+      this.show = true;
+    }
+  }
+  viewResults() {
+    if (this.percent >= 70) {
+      this.finalMessage = this.percent + "% \n PASS";
+      this.passedExam = true;
+    }
+    else {
+      this.finalMessage = this.percent + "% \n FAIL";
+      this.passedExam = false;
+    }
+    this.showExam = false;
+    this.showFinal = true;
+  }
+  showCorrect(qNum:string):boolean{
+    return this.qNumbers[parseInt(qNum)-1];
   }
 }
+
--- Phase2/Online-Exam/src/app/questions.model.ts
+++ Phase2/Online-Exam/src/app/questions.model.ts
@@ -1,5 +1,5 @@
 export class questions{
-    constructor(public question:string,public ans1:string,public ans2:string,public ans3:string,public ans4:string,public correct:string){
+    constructor(public question:string,public ans1:string,public ans2:string,public ans3:string,public ans4:string,public correct:string,public number:string){
 
     }
 }
